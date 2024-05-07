fdb2cb2584b16d3ac59747d1a0d816f5f0120741
Irfad G <irfad.g@direction.biz>
2024-05-07 16:53:43 +0530

    re architecting folders make project more proffehnal-phase1

--- lib/presentation/main_screen/widget/bottomNavigationBar.dart
+++ lib/presentation/main_screen/widget/bottomNavigationBar.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:learning_app/core/colors.dart';
+
+import '../../../helper/colors.dart';
+
 
 
 ValueNotifier<int> indexChangeNotfyer = ValueNotifier(0);
